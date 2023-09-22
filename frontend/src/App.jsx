bf3c05693a89b0c62c22be3d3b3404c1ece7fc7a
Sahil Makadia <makadiasahil9846@gmail.com>
2023-09-22 10:28:33 +0530

    Frontend Setup with Chakra UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <div>
+      <Button colorScheme="cyan" textColor={'white'}>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
